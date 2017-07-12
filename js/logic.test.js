f7ae66b5ce2104bfce3c144c874c81abd4e4dbca
Rebecacalvoquintero <rebecacalvoquintero@hotmail.es>
2017-07-12 16:38:08 +0100

    tests for makeGiphyUrl function relates to #14

--- js/logic.test.js
+++ js/logic.test.js
@@ -16,18 +16,26 @@ test ('formatLineName test', function(t){
 });
 
 
-test ('Return strings', function(t){
+test ("makeTflUrl test", function(t){
   var actual = typeof logic.makeTflUrl("Victoria Line");
   var expected = 'string';
   t.equal(actual,expected,'logic.makeTflUrl should return a string');
-  t.end();
 
-});
-
-test ('Return the correct url', function(t){
   var actual = logic.makeTflUrl("Victoria Line");
   var expected = 'https://api.tfl.gov.uk/Line/victoria/Status?app_id=de9e1a2e&app_key=41bcfcc2d033bae16403b619c8ec1613';
   t.equal(actual,expected,'logic.makeTflUrl should return the url');
-  t.end();
 
+  t.end();
+});
+
+test ("makeGiphyUrl test", function(t){
+  var actual = typeof logic.makeGiphyUrl(logic.resultsObj.status);
+  var expected = 'string';
+  t.equal(actual,expected,'logic.makeGiphyUrl should return a string');
+
+  var actual = logic.makeGiphyUrl(logic.resultsObj.status);
+  var expected = "https://api.giphy.com/v1/gifs/search?api_key=03d284987c9444e8931acbc0601067d3&q=aardvark&limit=25&offset=0&rating=G&lang=en";
+  t.equal(actual,expected,'logic.makeGiphyUrl should return the url');
+
+  t.end();
 });
