278ddd68548c83f2c24db01c538e0c51d54b8b8f
Max Gerber <mgerber94@gmail.com>
2017-07-13 11:21:15 +0100

    Written render function and removed from iife, wrote target html section
    Relates #19

--- js/dom.js
+++ js/dom.js
@@ -1,10 +1,13 @@
-var domRender = ( function() {
-      var render = function( resultsObject ) {
-      // stuff stuff stuff
-      console.log("hey");
-    };
-    return render;
-  }() );
+var render = function( resultsObject ) {
+  var lineTitleSelector = document.getElementsByClassName('selected-line')[0];
+  var imageSelector = document.getElementsByClassName('giphy')[0];
+  var captionSelector = document.getElementsByClassName('status-severity')[0];
+
+  lineTitleSelector.textContent = resultsObject.line;
+  imageSelector.src = resultsObject.url;
+  imageSelector.setAttribute('alt', resultsObject.status);
+  captionSelector.textContent = resultsObject.status;
+  };
 
 ( function() {
   var submit = document.getElementById('submit'),
