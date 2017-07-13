399dbba54dd7741d0a7044ea96c9d2c28e6d7eb3
Bart Bucknill <bartbucknill@hotmail.com>
2017-07-13 17:40:59 +0100

    add lines object
    #39

---
+++ js/lines.js
@@ -0,0 +1,21 @@
+var linesObj = {
+  "bakerloo": "Bakerloo",
+  "central": "Central",
+  "circle": "Circle",
+  "district": "District",
+  "hammersmith-city": "Hammersmith & City",
+  "jubilee": "Jubilee",
+  "metropolitan": "Metropolitan",
+  "northern": "Northern",
+  "piccadilly": "Piccadilly",
+  "victoria": "Victoria",
+  "waterloo-city": "Waterloo & City",
+  "london-overground": "London Overground",
+  "dlr": "DLR",
+  "tfl-rail": "TFL Rail",
+  "tram": "Tram"
+};
+
+if (typeof module !== 'undefined'){
+  module.exports=linesObj;
+}
