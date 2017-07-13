453e3d4cf2aa409ea94f075788f24e470bbbecbb
Bart Bucknill <bartbucknill@hotmail.com>
2017-07-13 15:25:47 +0100

    first attempt at waterfall function

--- js/logic.js
+++ js/logic.js
@@ -10,11 +10,11 @@ var logicObj = {
     return line.slice(0,1).toLowerCase() + line.slice(1,-5);
   },
 
-  makeTflUrl: function(line) {
+  makeTflUrl: function() {
     /// code!
     var urlStart = 'https://api.tfl.gov.uk/Line/';
     var urlEnd = '/Status?app_id=de9e1a2e&app_key=41bcfcc2d033bae16403b619c8ec1613';
-    return urlStart + logicObj.formatLineName(line) + urlEnd;
+    return urlStart + logicObj.formatLineName(logicObj.line) + urlEnd;
   },
 
   makeGiphyUrl: function() {
@@ -42,7 +42,7 @@ var logicObj = {
     var lineStatus = tflData[0].lineStatuses[0].statusSeverityDescription;
     logicObj.resultsObj.status = lineStatus;
     /// calls makeGiphyRequest
-    logicObj.makeGiphyRequest();
+    // logicObj.makeGiphyRequest();
   },
 
   giphyCb: function(giphyData) {
@@ -51,24 +51,46 @@ var logicObj = {
     var gifSrc = giphyData.data[randomNum].images.fixed_height.url;
     logicObj.resultsObj.url = gifSrc;
     /// call dom render
-    render(logicObj.resultsObj);
+    // render(logicObj.resultsObj);
   },
 
-  makeTflRequest: function(line) {
-    logicObj.resultsObj.line = line;
-    //calls makeTflUrl
-    var url = logicObj.makeTflUrl(line);
-    // calls apiCall with url and tflCb
-    logicObj.apiCall(url, logicObj.tflCb);
+  waterfall: function(tasksArray, urlsArray) {
+      if (tasksArray.length > 0) {
+
+          var task = tasksArray.slice(0,1);
+          var remainingTasks = tasksArray.slice(1);
+          var url = urlsArray.slice(0,1)();
+          var remainingUrls = urlsArray.slice(1);
+
+          logicObj.apiCall(url, function(data) {
+              task(data);
+              waterfall(remainingTasks, remainingUrls);
+          })
+
+      }
+      render(logicObj.resultsObj);
   },
 
-  makeGiphyRequest: function() {
-    //get line status from resultsObj - don't need to, as makeGiphyUrl doesn't take it as an argument and instead calls it from within the function
-    //calls makegiphyUrl
-    var url = logicObj.makeGiphyUrl();
-    //apiCall(url, giphyCb)
-    logicObj.apiCall(url, logicObj.giphyCb)
+  init: function(line) {
+      logicObj.line = line;
+      waterfall([tflCb, giphyCb], [makeTflUrl, makeGiphyUrl]);
   }
+
+//   makeTflRequest: function(line) {
+//     logicObj.resultsObj.line = line;
+//     //calls makeTflUrl
+//     var url = logicObj.makeTflUrl(line);
+//     // calls apiCall with url and tflCb
+//     logicObj.apiCall(url, logicObj.tflCb);
+//   },
+
+//   makeGiphyRequest: function() {
+//     //get line status from resultsObj - don't need to, as makeGiphyUrl doesn't take it as an argument and instead calls it from within the function
+//     //calls makegiphyUrl
+//     var url = logicObj.makeGiphyUrl();
+//     //apiCall(url, giphyCb)
+//     logicObj.apiCall(url, logicObj.giphyCb)
+//   }
 
 }
 
