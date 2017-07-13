e08a756d32302f67cf29f5f510b33bcb9d6b951d
Max Gerber <mgerber94@gmail.com>
2017-07-13 11:48:43 +0100

    Fixed browser/node conflict with require

--- js/dom.js
+++ js/dom.js
@@ -1,20 +1,20 @@
-var render = function( resultsObject ) {
+var render = function( resultsObj ) {
   var lineTitleSelector = document.getElementsByClassName('selected-line')[0];
   var imageSelector = document.getElementsByClassName('giphy')[0];
   var captionSelector = document.getElementsByClassName('status-severity')[0];
 
-  lineTitleSelector.textContent = resultsObject.line;
-  imageSelector.src = resultsObject.url;
-  imageSelector.setAttribute('alt', resultsObject.status);
-  captionSelector.textContent = resultsObject.status;
+  lineTitleSelector.textContent = resultsObj.line;
+  imageSelector.src = resultsObj.url;
+  imageSelector.setAttribute('alt', resultsObj.status);
+  captionSelector.textContent = resultsObj.status;
   };
 
 ( function() {
-var submit = document.getElementById('submit'),
+var submit = document.getElementsByTagName('form')[0],
     tubeLines = document.getElementsByName('tube-lines')[0];
 
-      submit.addEventListener( 'click', function( e ) {
+      submit.addEventListener( 'submit', function( e ) {
         e.preventDefault();
-        logicObject.makeTflRequest( tubeLines.value );
+        logicObj.makeTflRequest( tubeLines.value );
       });
 }() );
