83bb0437b6d2d6856d0d116ec2d9505ffd1208d6
Max Gerber <mgerber94@gmail.com>
2017-07-13 10:51:48 +0100

    skeleton for render function, seperated dom into two iifes Relates #7

--- js/dom.js
+++ js/dom.js
@@ -1,13 +1,17 @@
-( function() {
-    var submit = document.getElementById('submit'),
-        tubeLines = document.getElementsByName('tube-lines')[0],
-        render = function(resultsObject) {
+var domRender = ( function() {
+      var render = function( resultsObject ) {
+      // stuff stuff stuff
+      console.log("hey");
     };
-
-    submit.addEventListener( 'submit', function( el ) {
-      el.preventDefault();
-      logicObject.makeTflRequest( tubeLines.value );
-    });
-
     return render;
-}) ();
+  }() );
+
+( function() {
+  var submit = document.getElementById('submit'),
+      tubeLines = document.getElementsByName('tube-lines')[0];
+
+      submit.addEventListener( 'submit', function( el ) {
+        el.preventDefault();
+        logicObject.makeTflRequest( tubeLines.value );
+      });
+}() );
